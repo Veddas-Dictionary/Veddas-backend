704461b2b3cc21d51074edd3e1a7bacf0ee818bc
Such19 <32512017+Such19@users.noreply.github.com>
2021-04-27 18:57:02 +0530

    ignored config

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Job alerts of Member directory"});
+    res.json({"message": "Welcome to Veddas Dictionary Backend"});
 });
 
 require('./app/routes/job.routes.js')(app);
